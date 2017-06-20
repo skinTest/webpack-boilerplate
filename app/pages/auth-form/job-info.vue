9e956b75402dc49c63f0859b6d48403078a3953e
xuesongzhi@jrmf360.com <xuesongzhi@jrmf360.com>
2017-06-20 18:32:06 +0800

    职业信息

---
+++ app/pages/auth-form/job-info.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="">
+    <!-- 职业整体情况 -->
+    <div v-show="!activate_position_search">
+      <!-- form -->
+      <div class="weui-cells">
+        <at-static :cell="position_cell"></at-static>
+        <at-select :cell="income_cell"></at-select>
+        <at-select :cell="work_year_cell"></at-select>
+        <at-input :cell="test_cell"></at-input>
+      </div>
+
+      <!-- button -->
+      <div class="auth-bottom_btn_group">
+        <button
+          :disabled="!submit_valid"
+          :class="['weui-btn',
+                    submit_valid ? 'weui-btn_primary' : 'weui-btn_default']"
+          v-touch:tap="submit">
+          下一步
+        </button>
+      </div>
+    </div>
+
+    <!-- 职务具体选择 -->
+    <div class="action_panel" v-show="activate_position_search">
+      <job-search
+        ref="job_search"
+        v-on:position-change="position_change_handler">
+      </job-search>
+    </div>
+  </div>
+</template>
+
+<script type="text/javascript">
+import jobSearch from './job-search'
+
+export default {
+  components: {
+    jobSearch,
+  },
+  data: function () {
+    return {
+      position_cell: {
+        label: '职务',
+        is_link: true,
+        content: '',
+        value: '',
+        placeholder: '您在公司的职务或岗位',
+        name: 'position',
+        touch_handler: this.show_job_search.bind(this),
+      },
+      income_cell: {
+        label: '年收入',
+        name: 'income',
+        value: '',
+        placeholder: '请点击选择',
+        options: [],
+      },
+      work_year_cell: {
+        label: '工作年限',
+        name: 'work_year',
+        value: '',
+        placeholder: '请点击选择',
+        options: [],
+      },
+      test_cell: {
+        label: 'test',
+        placeholder: 'this test',
+        value: '',
+      },
+      activate_position_search: false,
+    }
+  },
+  computed: {
+    submit_valid: function () {
+      return true
+    },
+  },
+  methods: {
+    collect: function () {},
+    show_job_search: function () {
+      this.activate_position_search = true
+      var timer = setTimeout(function () {
+        this.$refs.job_search.focus()
+      }.bind(this), 300)
+    },
+    position_change_handler: function (item) {
+      this.position_cell.content = item.label
+      this.position_cell.value = item.value
+      this.test_cell.value = 100000000000
+
+      this.$nextTick(function () {
+        this.activate_position_search = false
+      })
+    },
+    submit: function () {
+      if (this.submit_valid) {
+        console.log('submit')
+      }
+    },
+  },
+  mounted: function () {
+  },
+}
+</script>
+
+<style lang="less">
+.action_panel {
+  position: fixed;
+  z-index: 500;
+  top: 0;
+  width: 100%;
+}
+</style>
---
+++ app/pages/auth-form/job-search.vue
@@ -0,0 +1,119 @@
+<template>
+<div class="page-wrapper">
+  <!-- 搜索框 -->
+  <div
+    class="weui-search-bar"
+    :class="{ 'weui-search-bar_focusing': is_focus }">
+    <form class="weui-search-bar__form">
+      <!-- input: show focus -->
+      <div class="weui-search-bar__box">
+        <i class="weui-icon-search"></i>
+        <input
+          ref="search_input"
+          v-model="value"
+          @input="input_hander"
+          placeholder="请输入关键词"
+          class="weui-search-bar__input"
+          type="search"/>
+        <!-- clear btn: show dirty -->
+        <a
+          v-touch:tap="clear_hander"
+          class="weui-icon-clear">
+        </a>
+      </div>
+
+      <!-- label: show blur -->
+      <label
+        v-touch:tap="focus"
+        class="weui-search-bar__label">
+        <i class="weui-icon-search"></i>
+        <span>搜索</span>
+      </label>
+    </form>
+    <a
+      v-touch:tap="close_search"
+      class="weui-search-bar__cancel-btn">取消</a>
+  </div>
+
+  <!-- 搜索结果 -->
+  <div class="weui-cells searchbar-result">
+    <div
+      v-for="(item, index) in search_result"
+      :key="index"
+      v-touch:tap="assign_result(item)"
+      class="weui-cell weui-cell_access">
+      <div class="weui-cell__bd weui-cell_primary">
+        <p>{{item.label}}</p>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script type="text/javascript">
+/*
+ * 1. 激活: 加 weui-search-bar_focusing
+ * 2. input: * 请求接口 * 清空
+ * 3. clear: 清空文字； 清空搜索结果
+ * 4. cancel: 去掉 weui-search-bar_focusing
+ * 5. 选择: 第一针改状态； 第二针赋值； 第三针恢复状态
+ */
+export default {
+  data: () => ({
+    is_focus: '',
+    value: '',
+    stop_search: true,
+    search_result: []
+  }), // end of data
+  watch: {
+    value: function () {
+      if (this.stop_search || this.value === '') {
+        this.search_result = []
+      }
+      else {
+        var result = '1,2,3,4'.split(',').map((val) => ({
+          label: `${val} ${this.value}`,
+          value: `${val} ${this.value}`,
+        }))
+
+        this.search_result = result
+      }
+    },
+  },
+  methods: {
+    focus: function () {
+      this.is_focus = true
+      this.$refs.search_input.focus()
+    },
+    input_hander: function () {
+      this.stop_search = false
+      console.log(this.value)
+    },
+    clear_hander: function () {
+      this.value = ''
+    },
+    assign_result: function (item) {
+      return function () {
+        this.stop_search = true
+        this.value = item.value
+        this.$emit('position-change', item)
+
+        this.$nextTick(function () {
+          this.stop_search = false
+        })
+      }.bind(this)
+    },
+    close_search: function () {
+      this.value = ''
+      this.is_focus = false
+      this.$emit('position-change', { label: '', value: ''})
+    },
+  },  // end of methods
+  mounted: function () {
+    var timer_id = setTimeout(function () {
+      this.focus()
+      clearTimeout(timer_id)
+    }.bind(this), 1000)
+  },
+}
+</script>
