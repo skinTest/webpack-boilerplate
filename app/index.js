20015d7fd42182872e13413f1105710bbc61198a
xuesongzhi@jrmf360.com <xuesongzhi@jrmf360.com>
2017-06-21 18:13:27 +0800

    render function

--- app/index.js
+++ app/index.js
@@ -5,9 +5,8 @@ import VueRouter from 'vue-router'
 import AtCells from './libs/at-cells'
 import touch from 'vue-directive-touch';
 
-/* --- 配置，组件 --- */
+/* --- 配置 --- */
 import { routes } from './pages/routes.js'
-import AppView from './pages/App.vue'
 
 /*
  * 注册 vue 插件
@@ -27,5 +26,7 @@ Vue.use(AtCells)
 //  启动全局的 vue isntance
 export const app = new Vue({
   router,
-  render: createElement => createElement(AppView),
+  render: function (createElement) {
+    return createElement('router-view')
+  },
 }).$mount('#app')
